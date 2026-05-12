8db633ed9ffd9000930bcab4c82a8edfb140d236
ricardo.castilloacevedo@ucr.ac.cr <ricardo.castilloacevedo@ucr.ac.cr>
2026-05-12 15:13:00 -0600

    Controlador Api react

---
+++ Inventario.SI/Controllers/ProductsApiController.cs
@@ -0,0 +1,23 @@
+﻿using Inventario.MODEL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventario.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsApiController : ControllerBase
+    {
+        private readonly BL.GestorInventario _gestor;
+
+        public ProductsApiController(BL.GestorInventario gestor)
+        {
+            _gestor = gestor;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Producto>> Get()
+        {
+            return Ok(_gestor.ObtenerProductos());
+        }
+    }
+}
